4b65baac5edfbb35df1d41081a2fc9b6f888cfdb
Erikmag24 <147947107+Erikmag24@users.noreply.github.com>
2023-11-04 02:53:01 +0100

    file_share.py

--- file_share.py
+++ file_share.py
@@ -1,15 +1,15 @@
 import requests
 
-# Imposta il tuo file da caricare
-file_path = 'path_al_tuo_file.txt'
+# Set the path to your file for uploading
+file_path = 'your_file_path.txt'
 
-# Esegui la richiesta POST per caricare il file su Transfer.sh
+# Make a POST request to upload the file to Transfer.sh
 response = requests.post('https://transfer.sh/', files={'file': open(file_path, 'rb')})
 
-# Verifica se la richiesta è andata a buon fine
+# Check if the request was successful
 if response.status_code == 200:
-    # Estrai il link di condivisione dalla risposta
+    # Extract the sharing link from the response
     share_link = response.content.decode('utf-8')
-    print(f"File caricato con successo. Link di condivisione: {share_link}")
+    print(f"File uploaded successfully. Sharing link: {share_link}")
 else:
-    print("Si è verificato un errore durante il caricamento del file.")
+    print("An error occurred while uploading the file.")
